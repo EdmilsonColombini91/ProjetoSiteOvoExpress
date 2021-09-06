9180cf0662fc56ba8a838db62bae499392253e9b
EdmilsonColombini91 <edmilson.colombini@gmail.com>
2021-09-06 03:21:48 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
